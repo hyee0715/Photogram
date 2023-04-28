4a45d773ab0ac0e802db4fb2cc22a95b608863d7
hyee0715 <hyee0715@naver.com>
2023-04-28 23:54:37 +0900

    feat: security 설정, view 세팅

---
+++ src/main/java/com/hy/photogram/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.hy.photogram.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //Security 활성화
+@Configuration //IoC
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //기존 Security의 기능을 모두 비활성화 시키고 기능을 직접 지정
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable() //csrf 토큰 비활성화
+            .authorizeRequests()
+                .antMatchers("/", "/user/**", "image/**", "/subscribe/**", "/comment/**").authenticated()   // 해당 경로로 접근하기 위해서는 권한이 필요함
+                .anyRequest().permitAll()   //이외의 경로에 대해서는 모두 허용
+            .and()
+            .formLogin()
+                .loginPage("/auth/signin")  //로그인 페이지 경로
+                .defaultSuccessUrl("/");    //로그인에 성공할 시 요청되는 경로
+    }
+}
